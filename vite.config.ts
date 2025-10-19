6bd63bc3432f84d09674b644783a1155d270c395
Tormod Wahl-Westreng <tormod.wahl-westreng@skatteetaten.no>
2025-10-19 23:15:46 +0200

    master: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/novemberlopet/' : './',
+  base: mode === 'production' ? '/novemberloepet/' : './',
   build: {
     rollupOptions: {
       input: {
