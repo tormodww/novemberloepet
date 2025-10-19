f44f151f9484046afb37eaab84f8b0464c284405
Tormod Wahl-Westreng <tormod.wahl-westreng@skatteetaten.no>
2025-10-19 21:27:12 +0200

    gh-pages: omskrevet routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_BASE || '/novemberloepet/';
-
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/novemberloepet/',
   build: {
     outDir: 'dist',
   },
