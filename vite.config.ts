b3a3d3a091007a25438f93ad835f669d64cfa27f
Tormod Wahl-Westreng <tormod.wahl-westreng@skatteetaten.no>
2025-10-19 20:09:43 +0200

    master: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash][extname]`
+      }
+    }
+  }
 })
